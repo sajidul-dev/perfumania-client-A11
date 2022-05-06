0809dcd63e3e04ff6275b4fc68e25f68b76d0f69
MdSajidulAlam <mdsajidulalam1245@gmail.com>
2022-05-07 01:39:34 +0600

    Added quantity implemented

--- src/components/ItemDetails/ItemDetails.js
+++ src/components/ItemDetails/ItemDetails.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import toast from 'react-hot-toast';
 import { useParams } from 'react-router-dom';
 
 const ItemDetails = () => {
+    const quantityRef = useRef('')
     const { itemId } = useParams()
     const [item, setItem] = useState([])
     useEffect(() => {
@@ -32,13 +33,48 @@ const ItemDetails = () => {
             })
 
     }
+    const addQuantity = (e) => {
+        e.preventDefault()
+        const newQuantity = quantityRef.current.value
+        const itemQuantity = item.quantity
+        const updateQuantity = parseInt(itemQuantity) + parseInt(newQuantity)
+        const quantity = { ...item, quantity: updateQuantity }
+        const url = `https://secure-retreat-97587.herokuapp.com/item/${item._id}`
+        fetch(url, {
+            method: "PUT",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(quantity)
+
+        })
+            .then(res => res.json())
+            .then(data => {
+                toast.success('Added Quantity', { id: 'Sajid' })
+                e.target.reset()
+            })
+
+    }
     return (
-        <div>
+        <div className='text-center'>
             <h1>Hello</h1>
             <img src={item.picture} alt="" />
+            <h3>Name: {item.name}</h3>
             <p>ID: {item._id}</p>
             <p>Quantity: {item.quantity}</p>
             <button onClick={() => handleDelivered(item._id)} className='btn btn-primary'>Delivered</button>
+
+
+
+            <form className='my-5' onSubmit={addQuantity}>
+                <label htmlFor="quantity">Restock {item.name}</label>
+                <br />
+                <input className='rounded p-2' ref={quantityRef} type="number" name="quantity" placeholder='Enter Quantity' />
+                <br />
+                <input className='my-2' type="submit" value="ADD" />
+            </form>
+
+
         </div>
     );
 };
